f0f186580e4ebf02c40036016c9fc5227d0ff0d4
Denissimo <droden@narod.ru>
2018-02-28 16:02:16 +0300

    Start

---
+++ Assets/Models/skeleton/Equipment/Hat/Hat.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hat : MonoBehaviour {
+	public bool active;
+	public static float r;
+	public static float g;
+	public static float b;
+	public static float a;
+	public Color color;
+	// Use this for initialization
+	void Start () {
+		r = 0.5F;
+		g = 0.1F;
+		b = 0.1F;
+		a = 1.0F;
+		color = new Color(r, g, b, a);
+		active = true;
+		//gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.SetActive (active);
+		//r = Random.Range (0.0F, 1.0F);
+		//g = Random.Range (0.0F, 1.0F);
+		//b = Random.Range (0.0F, 1.0F);
+		color = new Color(r, g, b, a);
+		//Debug.Log (gameObject.activeSelf);
+		GetComponent<Renderer>().material.color = color;
+	}
+}
---
+++ Assets/Standard Assets/Vehicles/Aircraft/Scripts/skeleton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skeleton : MonoBehaviour {
+	public GameObject hatr;
+	public GameObject vHelmet;
+	public GameObject katana;
+	public GameObject sword;
+
+	public bool hatrActive;
+	public bool vHelmetActive;
+	public bool katanaActive;
+	public bool swordActive;
+
+	// Use this for initialization
+	void Start () {
+		hatr = GameObject.Find("hatr");
+		vHelmet = GameObject.Find("vHelmet");
+		katana = GameObject.Find("katana");
+		sword = GameObject.Find("sword");
+		hatrActive = false;
+		vHelmetActive = true;
+		katanaActive = false;
+		Debug.Log (hatr);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hatr.SetActive (hatrActive);
+		vHelmet.SetActive (vHelmetActive);
+		katana.SetActive (katanaActive);
+		sword.SetActive (swordActive);
+	}
+}
